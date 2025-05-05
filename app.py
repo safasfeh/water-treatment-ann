814fc923138714e9ac2d5ea576b6b2aaa02bf0e6
safasfeh <aalsafasfeh@ttu.edu.jo>
2025-05-05 09:18:30 +0300

    app.py

--- app.py
+++ app.py
@@ -33,7 +33,9 @@ model = load_model('ann_water_model.h5')
 output_vars = [
     'Turbidity_final_NTU', 'Fe_final_mg_L', 'Mn_final_mg_L', 'Cu_final_mg_L',
     'Zn_final_mg_L', 'Suspended_solids_final_mg_L', 'TDS_final_mg_L',
-    'Turbidity_removal_%', 'Suspended_solids_removal_%', 'TDS_removal_%'
+    'Turbidity_removal_%', 'Suspended_solids_removal_%', 'TDS_removal_%','Coagulant_dose_mg_L',
+    'Flocculant_dose_mg_L', 'Mixing_speed_rpm',
+    'Rapid_mix_time_min', 'Slow_mix_time_min', 'Settling_time_min'
 ]
 
 # Standard reuse limits (with signs)
@@ -58,8 +60,6 @@ with st.form("input_form"):
         pH_raw = st.slider("pH of Raw Water", 3.0, 11.0, 7.0)
         turbidity_raw = st.slider("Turbidity (NTU)", 0.1, 500.0, 50.0)
         temperature = st.slider("Temperature (°C)", 5.0, 40.0, 25.0)
-        coagulant_dose = st.slider("Coagulant Dose (mg/L)", 0.0, 100.0, 30.0)
-        flocculant_dose = st.slider("Flocculant Dose (mg/L)", 0.0, 20.0, 5.0)
         fe_initial = st.slider("Initial Fe (mg/L)", 0.0, 10.0, 1.0)
         mn_initial = st.slider("Initial Mn (mg/L)", 0.0, 5.0, 0.3)
 
@@ -68,18 +68,14 @@ with st.form("input_form"):
         zn_initial = st.slider("Initial Zn (mg/L)", 0.0, 5.0, 0.1)
         ss = st.slider("Suspended Solids (mg/L)", 0.0, 1000.0, 150.0)
         tds = st.slider("TDS (mg/L)", 0.0, 5000.0, 1000.0)
-        mixing_speed = st.slider("Mixing Speed (rpm)", 50, 500, 200)
-        rapid_mix = st.slider("Rapid Mix Time (min)", 0.5, 10.0, 2.0)
-        slow_mix = st.slider("Slow Mix Time (min)", 1.0, 30.0, 10.0)
-        settling_time = st.slider("Settling Time (min)", 5.0, 60.0, 30.0)
 
     submitted = st.form_submit_button("Test Water Quality")
 
 # Prediction
 if submitted:
-    input_array = np.array([[pH_raw, turbidity_raw, temperature, coagulant_dose, flocculant_dose,
-                             fe_initial, mn_initial, cu_initial, zn_initial, ss, tds,
-                             mixing_speed, rapid_mix, slow_mix, settling_time]])
+    input_array = np.array([[pH_raw, turbidity_raw, temperature,
+                             fe_initial, mn_initial, cu_initial, zn_initial, ss, tds
+                            ]])
     
     X_scaled = scaler_X.transform(input_array)
     y_pred_scaled = model.predict(X_scaled)
@@ -88,7 +84,7 @@ if submitted:
     # Results formatting
     results = pd.DataFrame([y_pred], columns=output_vars).T
     results.columns = ['Predicted Value']
-    results['Unit'] = ['NTU', 'mg/L', 'mg/L', 'mg/L', 'mg/L', 'mg/L', 'mg/L', '%', '%', '%']
+    results['Unit'] = ['NTU', 'mg/L', 'mg/L', 'mg/L', 'mg/L', 'mg/L', 'mg/L', '%', '%', '%','mg/L','mg/L','rpm','min','min','min']
 
     assessments = []
     standards = []
